03a6daba11be18e81159412c7c40bc705331e1a2
Daryl Sabado <sabado114daryl@gmail.com>
2025-03-15 08:08:17 +0800

    remove depracated

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
