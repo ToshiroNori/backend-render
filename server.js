24804814fc8febf988b2a21ff5bff32318fa1c81
Daryl Sabado <sabado114daryl@gmail.com>
2025-03-16 00:36:05 +0800

    add origins for cors

--- server.js
+++ server.js
@@ -11,19 +11,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("MongoDB connection error:", err));
-
-// Middleware to parse JSON data
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
   })
 );
 
@@ -33,4 +26,9 @@ app.use("/api/auth", userRoutes);
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  // Connect to MongoDB
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("MongoDB connection error:", err));
 });
